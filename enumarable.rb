bae963afeb09cf0ac0766f7c5e0892e12477017a
Steven Jack Chung <stevenjchung12@gmail.com>
2020-10-12 17:24:31 -0500

    add my_each_with_index method

--- enumarable.rb
+++ enumarable.rb
@@ -1,9 +1,16 @@
-
+module Enumerable
   def my_each
     for element in self
     yield(element)
-    end 
+    end
   end
 
+  def my_each_with_index
+    i = 0
+    for element in self
+      yield(element, i)
+      i += 1
+    end
+  end
 
-
+end
