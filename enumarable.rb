cc374d72eb7c1eaae2cd00681039058ccfb066ac
thneves <tsneves11@gmail.com>
2020-10-13 17:28:28 -0300

    create my_map with procs, update README

--- enumarable.rb
+++ enumarable.rb
@@ -1,10 +1,8 @@
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
-    # this line is added because it returns an error when no block is given
-    # e.g. in irb when we use my_each with no block it gives an error of undefined local variable or method.
-
-    for element in self # for each element inside the array
+    
+    for element in self
     yield(element)
     end
   end
@@ -24,11 +22,6 @@ module Enumerable
 
     arr = []
     my_each { |element| arr.push(element) if yield(element)}
-    # my_each to iterate in the array and a block command where we say that element represent each element iterated, then we add the element to the array if the variable in yield is true
-
-    # e.g.
-    # [1,2,3,4,5,6].select { |n| n.even? }
-    # n represents yield(element), if yield(element) is even (true) then is added to the array.
 
     arr
   end
@@ -36,14 +29,10 @@ module Enumerable
   def my_all?(arg)
     if block_given?
       my_each { |element| return false if yield(element) == false}
-      # iterate each element in the array and see if it matches with yield(element) conditions
-      # e.g. [[1,2],2,'helo'].all? { |n| n.is_a?(Array) }
-      # the first element is an Array so it returns true, but the second element (2) is a numeric values, so returns false.
       return true
-      # returns true if everything matches
+
     elsif !arg.nil? and arg.is_a?(Class)
       my_each { |element| return false if element.class != arg}
-      # if argument is a class then checks if each elements matches with the class
     elsif arg.nil?
       return false
     else
@@ -94,10 +83,14 @@ module Enumerable
     end
 
     def my_map(arg = nil)
-      return to_enum(:my_map) unless block_given?
+      return to_enum(:my_map) unless block_given? || arg
 
       arr = []
-      my_each { |element| arr.push(yield(element)) }
+        if arg
+          my_each { |element| arr.push(arg.call(element)) }
+        else
+          my_each { |element| arr.push(yield(element)) }
+      end
       return arr
     end
 
@@ -126,6 +119,5 @@ module Enumerable
     def multiply_els(array)
       array.my_inject(:*)
     end
-
 
 end
